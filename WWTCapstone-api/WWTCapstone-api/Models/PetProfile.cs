ac5d9ef4aeaaeb65e6d802887b59fb81f2bf9388
Nicole Holder <holdern@wwt.com>
2020-08-30 12:15:26 -0500

    WIP models

---
+++ WWTCapstone-api/WWTCapstone-api/Models/PetProfile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WWTCapstone_api.Models
+{
+    public partial class PetProfile
+    {
+
+
+    }
+}
